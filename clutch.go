d515ab96599d30d5d77ec8292e434e73fd08446b
miller <millerkinlin@gmail.com>
2024-11-17 18:13:42 -0500

    Updated Querying from qdrant to get points and add to model context

--- clutch.go
+++ clutch.go
@@ -67,12 +67,17 @@ func main() {
 
 	// Perform a query to test the connection
 	fmt.Println("Querying the DB")
+	// jsonQuery := map[string]interface{}{
+	// 	"timestamp":    "2024-01-01T00:00:00Z",
+	// 	"machine_id":   "4",
+	// 	"machine_type": "planter",
+	// 	"location":     "field_1",
+	// 	"status":       "running",
+	// }
 	jsonQuery := map[string]interface{}{
-		"timestamp":    "2024-01-01T00:00:00Z",
 		"machine_id":   "4",
-		"machine_type": "planter",
-		"location":     "field_1",
 		"status":       "running",
+		"machine_type": "harvester",
 	}
 	jsonQueryString, err := json.Marshal(jsonQuery)
 	if err != nil {
@@ -81,7 +86,21 @@ func main() {
 	}
 	fmt.Println("JSON Query:", string(jsonQueryString))
 	docs := store.Query("clutch_testing_events", string(jsonQueryString))
-	fmt.Println("Query results:", docs)
+
+	// convert docs to string
+	docsString, err := json.Marshal(docs)
+	if err != nil {
+		fmt.Println("Error marshalling docs:", err)
+		return
+	}
+	docsStr := string(docsString)
+
+	// results := store.GetResults(docs)
+	// fmt.Println("Query results:", results)
+	model := cfg.Model
+	s, _ := model.QueryWithContext("what is the status of machine 4?", docsStr)
+	// First result: AI results: There are 2 machines currently operational at field_1, both planter machines with manufacturer "Testing" and model "ABCDE".
+	fmt.Println("AI results:", s)
 
 	// // Start the WebSocket here if you need to test events coming in and shit manually
 	// fmt.Println("Starting WebSocket server on :8080")
--- store/qdrant.go
+++ store/qdrant.go
@@ -118,20 +118,100 @@ func (s *QdrantStore) InsertDocument(index string, body map[string]interface{}) 
 	return
 }
 
+// func convertToQueryResultsToJson(res []*qdrant.BatchResult) (r map[string]interface{}) {
+// 	fmt.Println("---------- Converting to JSON ----------")
+// 	fmt.Println("Length of results:", len(res[0].GetResult()))
+// 	r = make(map[string]interface{})
+// 	for _, value := range res[0].GetResult() {
+// 		payload := value.GetPayload()
+// 		fmt.Println("Payload:", payload)
+// 		// Convert payload to json
+
+// 		jsonPayload, err := json.Marshal(payload)
+// 		if err != nil {
+// 			log.Printf("Error marshalling payload: %v", err)
+// 			return nil
+// 		}
+// 		fmt.Println("JSON Payload:", string(jsonPayload))
+// 		fmt.Println("================")
+
+//		}
+//		return nil
+//	}
+
+// func convertToJson(queryResults []*qdrant.BatchResult) (r map[string]interface{}) {
+// 	r = make(map[string]interface{})
+// 	if len(queryResults) == 0 || len(queryResults[0].GetResult()) == 0 {
+// 		return nil
+// 	}
+// 	results := make([]map[string]interface{}, 0)
+// 	qdrantResults := queryResults[0].GetResult()
+// 	for _, value := range qdrantResults {
+// 		payload := value.GetPayload()
+// 		payloadMap := make(map[string]interface{})
+// 	}
+// 	return nil
+// }
+
+func getQdrantValue(value *qdrant.Value) (r interface{}) {
+	switch val := value.GetKind().(type) {
+	case *qdrant.Value_StringValue:
+		return val.StringValue
+	case *qdrant.Value_IntegerValue:
+		return val.IntegerValue
+	case *qdrant.Value_BoolValue:
+		return val.BoolValue
+	case *qdrant.Value_StructValue:
+		return convertStructValueToJson(val.StructValue)
+	}
+	return nil
+}
+
+func convertStructValueToJson(structValue *qdrant.Struct) (r map[string]interface{}) {
+	r = make(map[string]interface{})
+	for k, v := range structValue.GetFields() {
+		r[k] = getQdrantValue(v)
+	}
+	return r
+}
+
+func convertToQueryResultsToJson(res []*qdrant.BatchResult) (r map[string]interface{}) {
+	r = make(map[string]interface{})
+	if len(res) == 0 || len(res[0].GetResult()) == 0 {
+		return nil
+	}
+
+	// Convert each result to a map
+	results := make([]map[string]interface{}, 0)
+	for _, value := range res[0].GetResult() {
+		payload := value.GetPayload()
+
+		// Convert Qdrant's payload to a regular map
+		payloadMap := make(map[string]interface{})
+		for k, v := range payload {
+			// Handle different value types from Qdrant
+			payloadMap[k] = getQdrantValue(v)
+		}
+		results = append(results, payloadMap)
+	}
+
+	r["hits"] = results
+	fmt.Println("---------- Done Converting to JSON ----------")
+	fmt.Println("JSON :", r)
+	fmt.Println("length of results:", len(results))
+	return r
+}
+
 func (s *QdrantStore) Query(collectionName string, query string) (r map[string]interface{}) {
-	// Filters
-	// ScoreThreshold
-	// Embedd Query
+
 	cfg := common.GetConfig()
 	model := cfg.Model
-	// Generate vector
+	// Generate vectorp
 	vector, err := model.GenerateEmbeddings(query)
 	if err != nil {
 		log.Printf("Error generating embeddings: %v", err)
 		return
 	}
-	// fmt.Println("Vector:", vector)
-	// search points
 
 	filter := qdrant.Filter{
 		Must: []*qdrant.Condition{
@@ -139,40 +219,44 @@ func (s *QdrantStore) Query(collectionName string, query string) (r map[string]i
 		},
 	}
 
+	limit := uint64(4)
+	scoreThreshold := float32(0.6)
+
+	queryPoints := &qdrant.QueryPoints{
+		CollectionName: collectionName,
+		Filter:         &filter,
+		Query:          qdrant.NewQuery(vector[0]...),
+		Limit:          &limit,
+		ScoreThreshold: &scoreThreshold,
+	}
+	queryPoints.WithPayload = &qdrant.WithPayloadSelector{
+		SelectorOptions: &qdrant.WithPayloadSelector_Enable{
+			Enable: true,
+		},
+	}
+
 	res, err := s.Client.QueryBatch(context.Background(), &qdrant.QueryBatchPoints{
 		CollectionName: collectionName,
-		QueryPoints: []*qdrant.QueryPoints{
-			{
-				CollectionName: collectionName,
-				Filter:         &filter,
-			},
-		},
+		QueryPoints:    []*qdrant.QueryPoints{queryPoints},
 	})
 	if err != nil {
 		log.Printf("Error querying collection: %v", err)
 		return nil
 	}
+	r = convertToQueryResultsToJson(res)
+	PrettyPrint(r["hits"].([]map[string]interface{}))
+	return r
+}
 
-	limit := uint64(3)
-	results, err := s.Client.Query(context.Background(), &qdrant.QueryPoints{
-		CollectionName: collectionName,
-		Query:          qdrant.NewQuery(vector[0]...),
-		// Filter: &qdrant.Filter{
-		// 	Should: []*qdrant.Condition{
-		// 		qdrant.NewMatch("machine_id", "4"),
-		// 	},
-		// },
-		Limit: &limit,
-	})
-	if err != nil {
-		panic(err)
+func PrettyPrint(v []map[string]interface{}) {
+	fmt.Println("---------- Pretty Print ----------")
+	for _, mp := range v {
+		fmt.Println("----------")
+		for k, v := range mp {
+			fmt.Println(k, v)
+		}
 	}
-
-	fmt.Println("Results: ", results)
-
-	return map[string]interface{}{
-		"results": res,
-	}
+	fmt.Println("---------- Done Pretty Print ----------")
 }
 
 func (s *QdrantStore) DeleteIndex(index string) {
@@ -182,9 +266,70 @@ func (s *QdrantStore) DeleteIndex(index string) {
 }
 
 func (s *QdrantStore) GetResults(searchResult map[string]interface{}) (res []map[string]interface{}) {
-	// For Qdrant, this might be a no-op since collections/indices are typically
-	// created during initialization
 	return nil
+	// getPoints := &qdrant.GetPoints{
+	// 	CollectionName: "clutch_testing_events",
+	// 	Ids:            []*qdrant.PointId{qdrant.NewIDNum(3)},
+	// }
+	// getPoints.WithPayload = &qdrant.WithPayloadSelector{
+	// 	SelectorOptions: &qdrant.WithPayloadSelector_Enable{
+	// 		Enable: true,
+	// 	},
+	// }
+	// // Get point 1 in collection
+	// point, err := s.Client.Get(context.Background(), getPoints)
+	// if err != nil {
+	// 	log.Printf("Error getting point: %v", err)
+	// 	return nil
+	// }
+	// fmt.Println("Point:", point)
+	// fmt.Println("Payload:", point[0].GetPayload())
+	// return nil
+	// // Take a json of {id: {num: X}} and get those results from Qdrant
+	// collectionName := searchResult["collection"].(string)
+	// // convert docs to []map[string]interface{}
+
+	// docs := searchResult["results"].([]*qdrant.BatchResult)
+	// fmt.Println("Search results:", docs)
+	// ids := []uint64{}
+	// ids_points := []*qdrant.PointId{}
+	// for _, doc := range docs {
+	// 	sc := doc.GetResult()
+	// 	for _, point := range sc {
+	// 		id := point.GetId()
+	// 		id_num := id.GetNum()
+	// 		uuid := id.GetUuid()
+	// 		// Skip if id_num is 0 and uuid is not empty
+	// 		if id_num == 0 && uuid != "" {
+	// 			continue
+	// 		} else {
+	// 			ids = append(ids, id_num)
+	// 			ids_points = append(ids_points, id)
+	// 		}
+	// 	}
+	// }
+	// fmt.Println("IDS:", ids)
+
+	// points, err := s.Client.Get(context.Background(), &qdrant.GetPoints{
+	// 	CollectionName: collectionName,
+	// 	Ids:            ids_points,
+	// })
+	// if err != nil {
+	// 	log.Printf("Error getting point: %v", err)
+	// 	return nil
+	// }
+	// fmt.Println("Points:", points)
+	// for _, point := range points {
+	// 	payload := point.GetPayload()
+	// 	fmt.Println("Payload:", payload)
+	// 	// convert payload to map[string]interface{}
+	// 	payloadMap := make(map[string]interface{})
+	// 	for k, v := range payload {
+	// 		payloadMap[k] = v
+	// 	}
+	// 	res = append(res, payloadMap)
+	// }
+	// return res
 }
 
 // func (s *QdrantStore) Test() {
